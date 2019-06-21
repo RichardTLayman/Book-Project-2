2e42c8e77980b445f3854a3593fea8d038659ce4
Richard Layman <richard.t.layman@gmail.com>
2019-06-21 11:21:40 -0400

    Add project files.

---
+++ Book Project/Controllers/MainPageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Project.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Intro()
+        {
+            return View();
+        }
+
+      
+    }
+}
